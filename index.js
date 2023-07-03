3d2d246638a9a12ac4b76028ef30ec69dc649938
SandeepKushwaha <sandeepkush327@gmail.com>
2023-07-03 20:30:20 +0530

    creating a contact list

--- index.js
+++ index.js
@@ -7,10 +7,37 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', function (request, response) { 
+let contactList = [
+    {
+        name: "Amit",
+        phone: "1234567890"
+    },
+    {
+        name: "Ram",
+        phone: "2345678901"
+    },
+    {
+        name: "Shyam",
+        phone: "3456789012"
+    },
+    {
+        name: "John",
+        phone: "4567890123"
+    },
+    {
+        name: "Tony Stark",
+        phone: "5678901234"
+    }
+];
+
+
+app.get('/', function (request, response) {
     console.log(__dirname);
     // response.send('<h1>Cool, it is running! or is it?</h1>');
-    return response.render('index', {title: "Contact List App"});
+    return response.render('index', {
+        title: "Contact List App",
+        contact_list: contactList
+    });
 });
 
 // creating the request for 'practice.ejs' file
@@ -23,6 +50,6 @@ app.get('/practice', function (request, response) {
 app.listen(port, function (err) {
     if (err)
         console.log('Error in running the server', err);
-    
+
     console.log('Yup! My Express Server is running on Port:', port);
 });
